29316a4c61d62af11cdffee8aaf177077717dbcf
iris1749 <jychoi1589@gmail.com>
2024-05-20 17:24:31 +0900

    blog main article card add

--- src/main/java/project/blog/MainController.java
+++ src/main/java/project/blog/MainController.java
@@ -2,11 +2,20 @@ package project.blog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 public class MainController {
-    @GetMapping("/blog")
-    public void index() {
-        System.out.println("index");
+
+    @GetMapping("/")
+    public String root() {
+        return "redirect:/blog_main";
     }
+
+    @GetMapping("/blog_main")
+    public String blogMain() {
+        return "blog_main";
+    }
+
 }
